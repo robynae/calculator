4d643951649d98f6c8efd68492c9cd85ad392a19
robynae <92494880+robynae@users.noreply.github.com>
2023-10-19 13:30:56 -0500

    Make basic styling of calculator

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
+let firstNumber;
+let secondNumber;
+let operator = '';
+
 function add(num1, num2) {
+    operator = 'add';
     return num1 + num2;
 }
 
 function subtract(num1, num2) {
+    operator = 'subtract';
     return num1 - num2;
 }
 
 function multiply(num1, num2) {
+    operator = 'multiply';
     return num1 * num2;
 }
 
 function divide(num1, num2) {
+    operator = 'divide'
     return num1 / num2;
 }
